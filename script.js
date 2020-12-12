6a92ee1bfd18941402abc8b6e46e0c63fa60c4b7
Ankit Kumar <47697582+Ankit8969@users.noreply.github.com>
2020-12-13 00:33:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,82 +1,170 @@
+// If you write any code do comment 
+// write the purpose why you add line
 
-// var array = "Ankit Kumar"
-var array = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore consequuntur ex iure minus provident voluptates aliquid dolor laboriosam debitis nam in sint maxime maiores suscipit eum aperiam, nobis qui delectus."
+// random array for typing leason
+var arr = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis repudiandae rem distinctio laudantium aperiam consequatur quia qui animi eos dolor quisquam mollitia dolore reiciendis voluptatibus officia adipisci, velit tenetur quasi?"
+
+var array = [] ;
+for (var i=0;i<arr.length;i++)
+{
+    if (arr[i] === " ")
+    array[i]="_"
+    else
+    array.push(arr[i])
+}
+
+// console.log(array)
+
+
+// select the typing box
 var typingBox = document.querySelector('.typing-leason')
+// Ready button section
 var readyBox = document.querySelector('.ready')
+// Selecting the body
 var bodyMain = document.querySelector('body')
+// initialize the color to false , because initially we don't know the user typed the key is correct or wrong 
 let color = false
+let space = 0;
 
+var resultBox = document.querySelector('.results')
 
+var startTime ,endTime , ss , es;
+
+// This is for the user, which key is typed by the user
 var typedString = ""
+var allTypedChar = ""
 
-console.log(array.length)
 
+// Putting every char of our array in span tag
 for (var i=0;i<array.length ;i++)
 {
     var htmlPart = `<span>${array[i]}</span>`
     typingBox.innerHTML += htmlPart;
 }
-
+// Selection of all the span tag
 var selectElement = document.querySelectorAll('span')
 
+// This is for , adding blinker to the first char 
 selectElement[typedString.length].classList.add('blinker')
 
-
+// This is for , Initially when the user not clicked on Ready button by using this all the char are shown in grey color
 var flag = true;
 if (flag)
 {
     typingBox.classList.add("inactive-leason")
 }
 
-typingBox.addEventListener('click', ()=>{
-    console.log("Clicked")
-})
-
+// adding the event on ready button 
 readyBox.addEventListener('click' , ()=>{
-    console.log("I got clicked")
     removeBox()
 })
 
+var f=0;
 function removeBox()
 {
+    // adding the hiding class on the ready button
     readyBox.classList.add("hiding")
+    // removing class from the typing leason
     typingBox.classList.remove("inactive-leason")
-    bodyMain.addEventListener('keypress', (event)=>{
-        var x = event.keyCode;  
-        var y = String.fromCharCode(x);
-        typedString += y;
 
-        const recentChar = selectElement[typedString.length-1]
-        
-        const originalString = selectElement[typedString.length-1].innerText
-    
-        if (typedString.length === array.length)
+    // adding the event class when the user click any keys
+    bodyMain.addEventListener('keypress', (event)=>{
+        // it gives the ASCII value of key
+        var x = event.keyCode;  
+        // it gives the char
+        var y = String.fromCharCode(x);
+        allTypedChar += y
+        typedString += y
+        console.log(typedString.length)
+
+        if (y === " ")
         {
-            readyBox.classList.remove("hiding")
+            allTypedChar += '_'
+        }
+        
+
+        // Timer start from , when the user press the first key
+        if (typedString.length === 1)
+        {
+            // Initialize the time 
+            let date=new Date()
+            startTime=date.getMinutes()
+            ss = date.getSeconds()
+            // console.log(startTime);
         }
 
-        if (y === originalString || x == 32 && originalString==='_') 
+        // Recent char which is pressed by the user
+        const recentChar = selectElement[typedString.length-1]
+        // Char which is present on typing leason
+        const originalString = selectElement[typedString.length-1].innerText
+        
+        if (typedString.length === array.length)
+        {
+            let date=new Date()
+            endTime=date.getMinutes()
+            es = date.getSeconds()
+            var time = ((endTime - startTime)*60 + Math.abs(ss - es))/60
+
+            // Minute;
+            var Mistake = (allTypedChar.length-array.length)-space
+            var speed =  (((typedString.length - Mistake)/5)/time)
+            var accuracy = (array.length/allTypedChar.length)*100
+
+            // console.log(time.toFixed(2), Mistake , speed.toFixed(2) , accuracy.toFixed(2))
+            var temp = `
+                    <div class="result">
+                        <div class="wpm">
+                        <h2>${speed.toFixed(2)}</h2>
+                        <p>Words / min.</p>
+                        </div>
+                        <div class="accuracy">
+                            <h2>${accuracy.toFixed(2)}%</h2>
+                            <p>Accuracy rate</p>
+                        </div>
+                        <div class="mistake">
+                            <h2>${Mistake}</h2>
+                            <p>Mistake</p>
+                        </div>
+                        <div class="minutes">
+                            <h2>${time.toFixed(2)}</h2>
+                            <p>Minutes</p>
+                        </div> 
+                    </div>`
+            resultBox.removeChild(resultBox.childNodes[0])
+            resultBox.innerHTML = temp
+        }
+        
+        if (originalString==='_')
+        space ++
+
+        // comparing the char , does it equal or not
+        if (y === originalString || x == 32 && originalString==='_')
         {
             if (color)
             {
                 selectElement[typedString.length-1].classList.add("wrong")
+                f=f+1;
                 color = false
             }
             else
             {
                 recentChar.classList.add("right")
             }
-            
+            // removing the blinker from the previous 
             selectElement[typedString.length-1].classList.remove('blinker')
+            // adding the blinker to the next one
+            if (typedString.length != array.length)
             selectElement[typedString.length].classList.add('blinker')
         }
         else
         {
-            // recentChar.classList.add("wrong")
             color=true
+            // removing the last char which was added to the typedString 
             typedString = typedString.substring(0, typedString.length - 1);
         }
-        
     })
+   
 }
+
+
 // recentChar.classList.add("wrong")
